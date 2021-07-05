ade0245699036303b45b27d5681dd86e236e5d15
nicky-eng <nicolasoyarzabal@gmail.com>
2021-07-05 19:44:17 -0300

    Half done job.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { StoreBoxComponent } from './store-box/store-box.component';
 import { StoreListComponent } from './store-list/store-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AddStoreComponent } from './add-store/add-store.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StoreBoxComponent,
-    StoreListComponent
+    StoreListComponent,
+    AddStoreComponent
   ],
   imports: [
     HttpClientModule,
